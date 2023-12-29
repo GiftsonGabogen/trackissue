88be44ccc559af2ccbbb639a4fbdc9b7bdd63e81
GiftsonGabogen <giftsongabogen@gmail.com>
2023-12-29 08:26:09 +0800

    displaying issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import "easymde/dist/easymde.min.css";
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { Controller, useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,47 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
+import prisma from "@/prisma/client";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
-    <Button>
-      <Link href="/issues/new">create new issue</Link>
-    </Button>
+    <div>
+      <Button>
+        <Link href="/issues/new">create new issue</Link>
+      </Button>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {!!issues &&
+            issues.map((issue) => (
+              <Table.Row key={issue.id}>
+                <Table.RowHeaderCell>
+                  {issue.title}
+                  <p className="md:hidden">{issue.status}</p>
+                </Table.RowHeaderCell>
+                <Table.Cell className="hidden md:table-cell">
+                  {issue.status}
+                </Table.Cell>
+                <Table.Cell className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
   );
 };
 
