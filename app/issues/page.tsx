61711379e18663fd4427e40e4852eebe815a38c8
GiftsonGabogen <giftsongabogen@gmail.com>
2023-12-29 16:04:50 +0800

    styled issue link using radix link

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,7 +3,7 @@ import { Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import { CreateNewIssueButton } from "../components/IssueActions";
-import Link from "next/link";
+import Link from "../components/Link";
 
 const delay = async () => {
   const newPromise = new Promise((resolve) => {
