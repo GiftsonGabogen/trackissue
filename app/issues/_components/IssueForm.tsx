6786efa8a20ce5e194948ff02192c332941e9720
GiftsonGabogen <giftsongabogen@gmail.com>
2023-12-30 08:29:11 +0800

    created edit issue page

---
+++ app/issues/_components/IssueForm.tsx
@@ -0,0 +1,83 @@
+"use client";
+import "easymde/dist/easymde.min.css";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
+import { ErrorMessage, Spinner } from "@/app/components";
+import { Issue } from "@prisma/client";
+
+type IssueFormData = z.infer<typeof createIssueSchema>;
+
+const IssueForm = ({ issue }: { issue?: Issue }) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const router = useRouter();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueFormData>({
+    resolver: zodResolver(createIssueSchema),
+  });
+
+  const [error, setError] = useState<string | null>(null);
+
+  const onFormSubmit = async (data: IssueFormData) => {
+    setIsSubmitting(true);
+    const res = await fetch("/api/issues", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      setError("unexpected behaviour");
+    } else {
+      router.push("/issues");
+    }
+    setIsSubmitting(false);
+  };
+
+  return (
+    <div className="max-w-xl space-y-4">
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form onSubmit={handleSubmit(onFormSubmit)} className="space-y-4">
+        <TextField.Root>
+          <TextField.Input
+            defaultValue={issue?.title}
+            placeholder="Title"
+            {...register("title")}
+          />
+        </TextField.Root>
+        <ErrorMessage>{errors.title?.message} </ErrorMessage>
+
+        <Controller
+          name="description"
+          control={control}
+          defaultValue={issue?.description}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+        <Button type="submit" disabled={isSubmitting}>
+          add issue {isSubmitting && <Spinner />}
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default IssueForm;
