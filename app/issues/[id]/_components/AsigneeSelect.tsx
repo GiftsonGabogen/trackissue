4a586871e46f915cddbf4af981d771044beb02b1
GiftsonGabogen <giftsongabogen@gmail.com>
2024-01-03 23:16:50 +0800

    populate assigneselect component with users from prisma

--- app/issues/[id]/_components/AsigneeSelect.tsx
+++ app/issues/[id]/_components/AsigneeSelect.tsx
@@ -1,13 +1,29 @@
+"use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AsigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetch("/api/users");
+      const fetchedUsers = (await data.json()) as User[];
+      setUsers(fetchedUsers);
+    };
+    getData();
+  }, []);
+
   return (
-    <Select.Root defaultValue="ebben">
+    <Select.Root>
       <Select.Trigger />
       <Select.Content position="popper">
-        <Select.Item value="ebben">Ebben</Select.Item>
-        <Select.Item value="gift">Gift</Select.Item>
+        {!!users.length &&
+          users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
       </Select.Content>
     </Select.Root>
   );
