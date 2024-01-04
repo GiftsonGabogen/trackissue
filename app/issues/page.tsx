f19b5d0987a3c6abb062794aa18ea0869c46020e
GiftsonGabogen <giftsongabogen@gmail.com>
2024-01-05 00:44:19 +0800

    added sorting functionality

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,23 +1,62 @@
-import React from "react";
+import NextLink from "next/link";
 import { Flex, Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
 import { CreateNewIssueButton, IssueStatusBadge, Link } from "@/app/components";
 import IssueStatusFilter from "./_components/IssueStatusFilter";
-import { useSearchParams } from "next/navigation";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+
+const IssuesList = ({ issues }: { issues: Issue[] }) => {
+  if (!!issues?.length) {
+    return issues.map((issue) => (
+      <Table.Row key={issue.id}>
+        <Table.RowHeaderCell>
+          <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+          <p className="md:hidden">
+            <IssueStatusBadge status={issue.status} />
+          </p>
+        </Table.RowHeaderCell>
+        <Table.Cell className="hidden md:table-cell">
+          <IssueStatusBadge status={issue.status} />
+        </Table.Cell>
+        <Table.Cell className="hidden md:table-cell">
+          {issue.createdAt.toDateString()}
+        </Table.Cell>
+      </Table.Row>
+    ));
+  }
+  return null;
+};
 
 const IssuesPage = async ({
   searchParams,
 }: {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
+  const tableHeaders: {
+    label: string;
+    value: keyof Issue;
+    className?: string;
+  }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
+  const orderBy = tableHeaders
+    .map((header) => header.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
   return (
     <div className="space-y-5">
@@ -28,34 +67,26 @@ const IssuesPage = async ({
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {tableHeaders.map((header) => (
+              <Table.ColumnHeaderCell
+                key={header.value}
+                className={header.className}
+              >
+                <NextLink
+                  href={{ query: { ...searchParams, orderBy: header.value } }}
+                >
+                  {header.label}
+                </NextLink>
+                {header.value === searchParams?.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
         <Table.Body>
-          {!!issues &&
-            issues.map((issue) => (
-              <Table.Row key={issue.id}>
-                <Table.RowHeaderCell>
-                  <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                  <p className="md:hidden">
-                    <IssueStatusBadge status={issue.status} />
-                  </p>
-                </Table.RowHeaderCell>
-                <Table.Cell className="hidden md:table-cell">
-                  <IssueStatusBadge status={issue.status} />
-                </Table.Cell>
-                <Table.Cell className="hidden md:table-cell">
-                  {issue.createdAt.toDateString()}
-                </Table.Cell>
-              </Table.Row>
-            ))}
+          <IssuesList issues={issues} />
         </Table.Body>
       </Table.Root>
     </div>
