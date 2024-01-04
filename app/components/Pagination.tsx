ee0472790dbd8295a4ba440a80216f0beb621a6b
GiftsonGabogen <giftsongabogen@gmail.com>
2024-01-05 02:25:47 +0800

    implemented pagination

---
+++ app/components/Pagination.tsx
@@ -0,0 +1,72 @@
+"use client";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { Button, Flex, Text } from "@radix-ui/themes";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface PaginationProps {
+  itemCount: number;
+  pageSize: number;
+  currentPage: number;
+}
+
+const Pagination = ({ itemCount, pageSize, currentPage }: PaginationProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pageCount = Math.ceil(itemCount / pageSize);
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", page.toString());
+    router.push(`?${params.toString()}`);
+  };
+
+  if (pageCount <= 1) return null;
+  return (
+    <Flex gap="4" justify="center" align="center">
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
+        <DoubleArrowLeftIcon />
+      </Button>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
+        <ChevronLeftIcon />
+      </Button>
+      <Text size="2">
+        Page {currentPage} of {pageCount}
+      </Text>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
+        <ChevronRightIcon />
+      </Button>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
+        <DoubleArrowRightIcon />
+      </Button>
+    </Flex>
+  );
+};
+
+export default Pagination;
