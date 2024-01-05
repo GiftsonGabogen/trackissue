413e9c667028c560bb9e0c75db75450be20a256e
GiftsonGabogen <giftsongabogen@gmail.com>
2024-01-05 19:36:56 +0800

    add Issue Summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const issues = await prisma.issue.findMany();
+  const openIssuesCount = issues.filter(
+    (issue) => issue.status === "OPEN"
+  ).length;
+  const closedIssuesCount = issues.filter(
+    (issue) => issue.status === "CLOSED"
+  ).length;
+  const inProgressIsssuesCount = issues.filter(
+    (issue) => issue.status === "IN_PROGRESS"
+  ).length;
+  return (
+    <IssueSummary
+      closed={closedIssuesCount}
+      open={openIssuesCount}
+      inProgress={inProgressIsssuesCount}
+    />
+  );
 }
